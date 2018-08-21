8dae3e7c5d225fd902868363da705b182340791b
osman.demirci <osman.demirci@SPL106>
2018-08-21 16:58:00 +0300

    mvc project

---
+++ Models/ResistanceCreateModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace ResistanceSurvey.Models
+{
+    public class ResistanceCreateModel
+    {
+        public int CompanyId { get; set; }
+        public DateTime StartDate { get; set; } 
+    }
+}
